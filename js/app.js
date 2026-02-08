a5b9a03c898dcf6fb2a8df08e1d8db564f23bcd7
guragaidipesh <guragaindipesh89@gmail.com>
2026-02-08 19:50:27 +0545

    checked/unchecked added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,13 @@ import { groceryItems } from "./data.js";
 import { createItems } from "./items.js";
 
 let items = groceryItems;
+// Edit Completed Function
+export function editCompleted(itemId) {
+  items = items.map((item) =>
+    item.id === itemId ? { ...item, completed: !item.completed } : item
+  );
+  render();
+}
 
 // Render App
 function render() {
--- js/single-item.js
+++ js/single-item.js
@@ -1,20 +1,28 @@
+import { editCompleted } from "./app.js";
+
 // Create SingleItem Element
 export function createSingleItem(item) {
   const div = document.createElement("div");
   div.className = "single-item";
 
   div.innerHTML = `
-      <input type="checkbox" ${item.completed ? "checked" : ""} />
-      <p style="text-decoration: ${item.completed ? "line-through" : "none"}">
-        ${item.name}
-      </p>
-      <button class="btn icon-btn edit-btn" type="button">
-        <i class="fa-regular fa-pen-to-square"></i>
-      </button>
-      <button class="btn icon-btn remove-btn" type="button">
-        <i class="fa-regular fa-trash-can"></i>
-      </button>
-    `;
+    <input type="checkbox" ${item.completed ? "checked" : ""} />
+    <p style="text-decoration: ${item.completed ? "line-through" : "none"}">
+      ${item.name}
+    </p>
+    <button class="btn icon-btn edit-btn" type="button">
+      <i class="fa-regular fa-pen-to-square"></i>
+    </button>
+    <button class="btn icon-btn remove-btn" type="button">
+      <i class="fa-regular fa-trash-can"></i>
+    </button>
+  `;
+
+  // ✅ Toggle completed
+  const checkbox = div.querySelector('input[type="checkbox"]');
+  checkbox.addEventListener("change", () => {
+    editCompleted(item.id);
+  });
 
   return div;
 }
